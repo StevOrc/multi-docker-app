82100d014e5dd5117354c760e7f07700bd166ef0
StevOrc <steven.orcun2@gmail.com>
2021-01-26 22:09:32 +0100

    test K8C changes

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
+        <div>WELCOME TEST K8C !!</div>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
